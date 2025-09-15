dd8010a0832c6a442e06602651dabba0c8d89360
avazzades <64379185+avazzades@users.noreply.github.com>
2025-09-14 22:26:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
+import { LandingPage } from "./components/LandingPage";
+import "./i18n"; // Initialize i18n
 
 function App() {
-  return <h1>Welcome to Tax Landing Page</h1>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
